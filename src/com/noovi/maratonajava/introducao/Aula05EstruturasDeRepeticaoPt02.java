8d32152f3c3dcf6a727d1970dda52f7224d12ea8
Paulo Moreira <pmoreira@noovi.com.br>
2025-07-03 15:14:27 -0300

    Removendo variaveis desnecessárias

--- src/com/noovi/maratonajava/introducao/Aula05EstruturasDeRepeticaoPt02.java
+++ src/com/noovi/maratonajava/introducao/Aula05EstruturasDeRepeticaoPt02.java
@@ -1,11 +1,10 @@
 package com.noovi.maratonajava.introducao;
 
-public class EstruturasDeRepeticao02 {
+public class Aula05EstruturasDeRepeticaoPt02 {
     public static void main(String[] args) {
         //Imprima todos os números pares de 0 até 1000000
-        int contador = 0;
 
-        for(int i = 3; i <= 1000000; i++) {
+        for (int i = 3; i <= 1000000; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
